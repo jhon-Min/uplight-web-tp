a4c1aaffba431b48d1f43f28f97a9ff57be672c2
seojun <minnyisay@gmail.com>
2021-12-07 14:48:06 +0630

    micro interaction

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,15 +1,58 @@
 $(document).ready(function () {
   let screenHeight = $(window).height();
-  console.log(screenHeight);
 
   $(window).scroll(function () {
     let current = $(this).scrollTop();
-    // console.log(current);
-
-    if (current > screenHeight - 100) {
+    console.log(current);
+    if (current > screenHeight - 105) {
       $(".navbar").addClass("site-nav");
     } else {
       $(".navbar").removeClass("site-nav");
+      setActive("home");
     }
   });
 });
+
+$(".navbar-toggler").click(function () {
+  let result = $(".navbar-collapse").hasClass("hello");
+
+  if (result) {
+    $(".menu-icon").removeClass("feather-menu").addClass("feather-x");
+    $(".navbar-collapse").removeClass("hello");
+  } else {
+    $(".menu-icon").removeClass("feather-x").addClass("feather-menu");
+    $(".navbar-collapse").addClass("hello");
+  }
+});
+
+function setActive(x) {
+  $(".nav-link").removeClass("nav-active");
+  $(`.nav-link[href='#${x}']`).addClass("nav-active");
+}
+
+function navScroll() {
+  let currentSection = $("div[id]");
+  currentSection.waypoint(
+    function (direction) {
+      if (direction == "down") {
+        let currentSection = $(this.element).attr("id");
+        console.log("down", currentSection);
+        setActive(currentSection);
+      }
+    },
+    { offset: "70px" }
+  );
+
+  currentSection.waypoint(
+    function (direction) {
+      if (direction == "up") {
+        let currentSection = $(this.element).attr("id");
+        console.log("up", currentSection);
+        setActive(currentSection);
+      }
+    },
+    { offset: "70px" }
+  );
+}
+
+navScroll();
